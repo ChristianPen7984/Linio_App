65f10c289291c8282f255871923d410636cbe830
christian212519 <penrod.christian7984@gmail.com>
2019-05-02 19:34:03 -0400

    Created splash screen and navigation drawer

--- app/src/main/java/com/app/linio_app/MainActivity.java
+++ app/src/main/java/com/app/linio_app/MainActivity.java
@@ -1,13 +1,100 @@
 package com.app.linio_app;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.app.linio_app.Fragments.Home;
+import com.app.linio_app.Fragments.Login;
+import com.app.linio_app.Fragments.Panels;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DrawerLayout drawer;
+    NavigationView navigation;
+    ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawer = findViewById(R.id.drawer);
+        navigation = findViewById(R.id.navigation);
+        navigation.setNavigationItemSelectedListener(this);
+        initNavView();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                if (drawer.isDrawerOpen(navigation)) {
+                    drawer.closeDrawers();
+                } else drawer.openDrawer(navigation);
+                break;
+
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        switch (menuItem.getItemId()) {
+            case R.id.home:
+                transaction.replace(R.id.fragmentContainer,new Home());
+                setActionBarTitle("HOME");
+                drawer.closeDrawers();
+                break;
+            case R.id.panels:
+                transaction.replace(R.id.fragmentContainer,new Panels());
+                setActionBarTitle("PANELS");
+                drawer.closeDrawers();
+                break;
+            case R.id.logout:
+                transaction.replace(R.id.fragmentContainer,new Login());
+                setActionBarTitle("LINIO");
+//                auth.signOut();
+                hideNonAuthLinks();
+                drawer.closeDrawers();
+                break;
+        }
+        transaction.commit();
+        return false;
+    }
+
+    private void initNavView() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        actionBarDrawerToggle = new ActionBarDrawerToggle(MainActivity.this,drawer,0,0);
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+    }
+
+    private void setActionBarTitle(String title) {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setHomeButtonEnabled(true);
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(title);
+        }
+    }
+
+    private void hideNonAuthLinks() {
+        Menu menu = navigation.getMenu();
+        menu.findItem(R.id.panels).setVisible(false);
     }
 }
--- app/src/main/java/com/app/linio_app/SplashScreen.java
+++ app/src/main/java/com/app/linio_app/SplashScreen.java
@@ -1,7 +1,11 @@
 package com.app.linio_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.concurrent.TimeUnit;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        launchSplashScreen();
+    }
+    private void launchSplashScreen() {
+        Thread timer = new Thread() {
+            @Override
+            public  void run() {
+                try {
+                    TimeUnit.MILLISECONDS.sleep(1000);
+                } catch (Exception e) {
+                    Toast.makeText(SplashScreen.this,"Unable to load splash screen",Toast.LENGTH_LONG).show();
+                }
+                Intent main = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(main);
+            }
+        };
+        timer.start();
     }
 }
